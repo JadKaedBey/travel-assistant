e4ae76755e45f310cc06165f12ae9d5d166b8de2
Ruben Vandamme <ruben.vandamme@ugent.be>
2024-11-05 19:44:53 +0200

    linting

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -1,10 +1,18 @@
 /* Interactions with the FastAPI backend */
 import axios from 'axios';
 
+interface CheckSessionIdResponse {
+  found: boolean;
+}
+
+interface FreshSessionIdResponse {
+  session_id: string;
+}
+
 const apiService = {
   sendQuery: async (query: string, sessionId: string) => axios.post('http://localhost:8000/query/', { user_input: query, session_id: sessionId }),
-  checkSessionId: async (sessionId: string) => axios.get('http://localhost:8000/check-session-id/', { params: { session_id: sessionId } }),
-  freshSessionId: async () => axios.post('http://localhost:8000/fresh-session-id/')
+  checkSessionId: async (sessionId: string) => axios.get<CheckSessionIdResponse>('http://localhost:8000/check-session-id/', { params: { session_id: sessionId } }),
+  freshSessionId:  async () => axios.post<FreshSessionIdResponse>('http://localhost:8000/fresh-session-id/')
 };
 
 export default apiService;
